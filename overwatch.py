1baf94df53522b0b898cf16142a6ee3a55d9c258
Piotr Bugaj <piotr2000bugaj@wp.pl>
2020-01-13 20:07:48 +0100

    Fixed a randomness prototype

--- overwatch.py
+++ overwatch.py
@@ -2,10 +2,11 @@ async def run_overwatch(ctx):
     from random import randrange
 los = randrange(5)
 if los==0:
-  await ctx.send("D.Va my waifu.1")
+  tresc = 1
 elif los==2:
-  await ctx.send("D.Va my waifu.2")
+  tresc = 2
 elif los==3:
-  await ctx.send("D.Va my waifu.3")
+  tresc = 3
 elif los==4:
-  await ctx.send("D.Va my waifu.4")
+  tresc = 4
+    await ctx.send(tresc)
