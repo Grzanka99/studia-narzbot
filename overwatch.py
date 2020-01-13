6117ab7801edac68ed31ff5ebf136d52951a22ff
Piotr Bugaj <piotr2000bugaj@wp.pl>
2020-01-13 20:05:02 +0100

    Add a randomness prototype

--- overwatch.py
+++ overwatch.py
@@ -1,2 +1,11 @@
 async def run_overwatch(ctx):
-    await ctx.send("D.Va my waifu.")
+    from random import randrange
+los = randrange(5)
+if los==0:
+  await ctx.send("D.Va my waifu.1")
+elif los==2:
+  await ctx.send("D.Va my waifu.2")
+elif los==3:
+  await ctx.send("D.Va my waifu.3")
+elif los==4:
+  await ctx.send("D.Va my waifu.4")
