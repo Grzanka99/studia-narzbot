fde6984b52740c30eba3049cfa92756655c4b346
Piotr Bugaj <piotr2000bugaj@wp.pl>
2020-01-13 20:10:56 +0100

    Fixed tabulation errors

--- overwatch.py
+++ overwatch.py
@@ -1,12 +1,13 @@
+from random import randrange
+
 async def run_overwatch(ctx):
-    from random import randrange
-los = randrange(5)
-if los==0:
-  tresc = 1
-elif los==2:
-  tresc = 2
-elif los==3:
-  tresc = 3
-elif los==4:
-  tresc = 4
+    los = randrange(5)
+    if los==0:
+        tresc = 1
+    elif los==2:
+        tresc = 2
+    elif los==3:
+        tresc = 3
+    elif los==4:
+        tresc = 4
     await ctx.send(tresc)
